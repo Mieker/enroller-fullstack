e7ee252cf2fa0fd62a5289270f3420d9df941e8f
Marcin <mieker@wp.pl>
2020-05-25 18:58:22 +0200

    add MeetingRestController.registerMeeting and MeetingService methods

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -2,6 +2,8 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Meeting;
 import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -10,9 +12,11 @@ import java.util.Collection;
 public class MeetingService {
 
     DatabaseConnector connector;
+    Session session;
 
     public MeetingService() {
         connector = DatabaseConnector.getInstance();
+        session = connector.getSession();
     }
 
     public Collection<Meeting> getAll() {
@@ -21,5 +25,14 @@ public class MeetingService {
         return query.list();
     }
 
+    public Meeting findById(long id) {
+        return (Meeting) session.get(Meeting.class, id);
+    }
 
+    public Meeting add(Meeting meeting) {
+        Transaction transaction = this.session.beginTransaction();
+        session.save(meeting);
+        transaction.commit();
+        return meeting;
+    }
 }
